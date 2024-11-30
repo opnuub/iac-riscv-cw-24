80836d8a866ba766eac7e4fe05505f53445c0d60
OpNUB <cody.liush@gmail.com>
2024-11-30 13:36:20 +0000

    modified and pushed PC section

---
+++ rtl/pcMux.sv
@@ -0,0 +1,11 @@
+module pcMux #(
+    parameter ADDR_WIDTH = 16
+) (
+    input   logic [ADDR_WIDTH-1:0]  branchPC,
+    input   logic [ADDR_WIDTH-1:0]  incPC,
+    input   logic                   PCsrc,
+    output  logic [ADDR_WIDTH-1:0]  nextPC
+);
+
+    assign nextPC = PCsrc ? branchPC : incPC;
+endmodule
