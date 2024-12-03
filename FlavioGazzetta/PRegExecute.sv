f93da5e80d309666fdf0dd5340ba3e8aa707ded2
FlavioGazzetta <flavio.gazzetta.2005@gmail.com>
2024-12-03 18:31:27 +0000

    completed pipelining registers including Control unit outputs

--- FlavioGazzetta/PRegExecute.sv
+++ FlavioGazzetta/PRegExecute.sv
@@ -10,7 +10,13 @@ module PRegDecode #(
     output  logic [DATA_WIDTH-1:0]  ALUResultM,
     output  logic [DATA_WIDTH-1:0]  WriteDataM,
     output  logic [4:0]             RdM,
-    output  logic [DATA_WIDTH-1:0]  PCPlus4M
+    output  logic [DATA_WIDTH-1:0]  PCPlus4M,
+    input   logic                   RegWriteE,
+    input   logic [1:0]             ResultSrcE,
+    input   logic                   MemWriteE,
+    output  logic                   RegWriteM,
+    output  logic [1:0]             ResultSrcM,
+    output  logic                   MemWriteM
 );
 
     logic [DATA_WIDTH-1:0] rom_array [2**REG_DATA_WIDTH-1:0];
@@ -21,22 +27,24 @@ module PRegDecode #(
     end
 
     always_comb begin
-
-        rom_array[3'b00] = ALUout;
-        rom_array[3'b01] = WriteData; 
-        rom_array[3'b10] = PCPlus4E;
-        rom_array[3'b11] = RdE;
-        
+        rom_array[3'b000] = ALUout;
+        rom_array[3'b001] = WriteData;
+        rom_array[3'b010] = PCPlus4E;
+        rom_array[3'b011] = {{(DATA_WIDTH-5){1'b0}}, RdE}; 
+        rom_array[3'b100] = {{(DATA_WIDTH-1){1'b0}}, RegWriteE}; 
+        rom_array[3'b101] = {{(DATA_WIDTH-2){1'b0}}, ResultSrcE};
+        rom_array[3'b110] = {{(DATA_WIDTH-1){1'b0}}, MemWriteE}; 
     end
 
    
     always_ff @(posedge clk) begin
-
-        ALUResultM <= rom_array[3'b00];
-        WriteDataM <= rom_array[3'b01];
-        RdM <= rom_array[3'b10];
-        PCPlus4M <= rom_array[3'b11];
-        
+        ALUResultM <= rom_array[3'b000];
+        WriteDataM <= rom_array[3'b001];
+        PCPlus4M   <= rom_array[3'b010];
+        RdM        <= rom_array[3'b011][4:0];  
+        RegWriteM  <= rom_array[3'b100][0];    
+        ResultSrcM <= rom_array[3'b101][1:0];  
+        MemWriteM  <= rom_array[3'b110][0];   
     end
 
 endmodule
--- FlavioGazzetta/PRegMemory.sv
+++ FlavioGazzetta/PRegMemory.sv
@@ -7,11 +7,14 @@ module PRegDecode #(
     input   logic [4:0]             RdM,
     input   logic [DATA_WIDTH-1:0]  PCPlus4M,
     input   logic                   clk,
-    input   logic [4:0]             RdW,
+    output  logic [4:0]             RdW,
     output  logic [DATA_WIDTH-1:0]  ALUResultW,
     output  logic [DATA_WIDTH-1:0]  WriteDataW,
-    output  logic [DATA_WIDTH-1:0]  RdW,
-    output  logic [DATA_WIDTH-1:0]  PCPlus4W
+    output  logic [DATA_WIDTH-1:0]  PCPlus4W,
+    input   logic                   RegWriteM,
+    input   logic [1:0]             ResultSrcM,
+    output  logic                   RegWriteW,
+    output  logic [1:0]             ResultSrcW
 );
 
     logic [DATA_WIDTH-1:0] rom_array [2**REG_DATA_WIDTH-1:0];
@@ -22,22 +25,22 @@ module PRegDecode #(
     end
 
     always_comb begin
-
-        rom_array[3'b00] = ALUResultM;
-        rom_array[3'b01] = DMRd; 
-        rom_array[3'b10] = RdM;
-        rom_array[3'b11] = PCPlus4M;
-        
+        rom_array[3'b000] = ALUResultM;
+        rom_array[3'b001] = DMRd;
+        rom_array[3'b010] = {{(DATA_WIDTH-5){1'b0}}, RdM};
+        rom_array[3'b011] = PCPlus4M;
+        rom_array[3'b100] = {{(DATA_WIDTH-1){1'b0}}, RegWriteM};
+        rom_array[3'b101] = {{(DATA_WIDTH-2){1'b0}}, ResultSrcM};
     end
 
    
     always_ff @(posedge clk) begin
-
-        ALUResultM <= rom_array[3'b00];
-        WriteDataM <= rom_array[3'b01];
-        RdM <= rom_array[3'b10];
-        PCPlus4M <= rom_array[3'b11];
-        
+        ALUResultW <= rom_array[3'b000];
+        WriteDataW <= rom_array[3'b001];
+        RdW        <= rom_array[3'b010][4:0];    
+        PCPlus4W   <= rom_array[3'b011];
+        RegWriteW  <= rom_array[3'b100][0];      
+        ResultSrcW <= rom_array[3'b101][1:0];    
     end
 
 endmodule
