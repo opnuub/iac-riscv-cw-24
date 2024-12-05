23d1370380066a884b31551e474afbbc38c68a5e
Michael Li <mcl123@ic.ac.uk>
2024-12-05 17:05:43 +0000

    Update imem&main decoder

---
+++ RISC-V_RV32I_Project/tb/tests/pcunit_tb.cpp
@@ -0,0 +1,87 @@
+#include <gtest/gtest.h>
+#include "Vpcunit.h"
+#include "verilated.h"
+
+class PcUnitTest : public ::testing::Test {
+protected:
+    Vpcunit* pcunit;
+
+    void SetUp() override {
+        pcunit = new Vpcunit;
+        pcunit->rst_ni = 0;  
+        pcunit->clk_i = 0;
+        pcunit->eval();
+        pcunit->rst_ni = 1;  // Release reset
+        pcunit->eval();
+    }
+
+    void TearDown() override {
+        delete pcunit;
+    }
+
+    void toggleClock() {
+        pcunit->clk_i = 1;
+        pcunit->eval();
+        pcunit->clk_i = 0;
+        pcunit->eval();
+    }
+};
+
+TEST_F(PcUnitTest, SequentialIncrement) {
+    pcunit->next_pc_sel_i = 0b000;
+    toggleClock();
+    ASSERT_EQ(pcunit->pc_o, 0x00000004);
+
+    toggleClock();
+    ASSERT_EQ(pcunit->pc_o, 0x00000008);
+}
+
+TEST_F(PcUnitTest, Branch) {
+    pcunit->next_pc_sel_i = 0b001;
+    pcunit->imm_i = 0x00000008;
+    toggleClock();
+    ASSERT_EQ(pcunit->pc_o, 0x00000008);
+}
+
+TEST_F(PcUnitTest, JumpAndLink) {
+    pcunit->next_pc_sel_i = 0b010;
+    pcunit->imm_i = 0x00000010;
+    toggleClock();
+    ASSERT_EQ(pcunit->pc_o, 0x00000010);
+}
+
+TEST_F(PcUnitTest, JumpAndLinkRegister) {
+    pcunit->next_pc_sel_i = 0b011;
+    pcunit->alu_result_i = 0x00000014;
+    toggleClock();
+    ASSERT_EQ(pcunit->pc_o, 0x00000014 & ~0x1);  // Align to the nearest even addr
+}
+
+TEST_F(PcUnitTest, ResetBehavior) {
+    pcunit->rst_ni = 0;  // Assert reset
+    pcunit->eval();
+    ASSERT_EQ(pcunit->pc_o, 0x00000000);
+
+    pcunit->rst_ni = 1;  // Deassert reset
+    pcunit->eval();
+    pcunit->next_pc_sel_i = 0b000;
+    toggleClock();
+    ASSERT_EQ(pcunit->pc_o, 0x00000004);
+}
+
+TEST_F(PcUnitTest, ResetBehavior){
+    pcunit->rst_ni = 0;
+    pcunit->eval();
+    ASSERT_EQ(pcunit->pc_o, 0x00000000);
+
+    pcunit->rst_ni = 1;
+    pcunit->eval();
+    pcunit->next_pc_sel_i = 0b000;
+    toggleClock();
+    ASSERT_EQ(pcunit->pc_o, 0x00000004);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
