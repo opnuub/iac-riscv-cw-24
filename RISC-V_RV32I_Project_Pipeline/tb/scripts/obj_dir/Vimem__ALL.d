811acce30d25eeef13acb504561229448411fa75
FlavioGazzetta <flavio.gazzetta.2005@gmail.com>
2024-12-06 01:51:08 +0000

    made futher changes to pipeline branch allowing for push

--- RISC-V_RV32I_Project_Pipeline/tb/scripts/obj_dir/Vimem__ALL.d
+++
@@ -1,9 +0,0 @@
-Vimem__ALL.o: Vimem__ALL.cpp Vimem.cpp Vimem.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h Vimem__Syms.h \
- Vimem___024root.h Vimem___024root__DepSet_hd637b107__0.cpp \
- Vimem__ConstPool_0.cpp Vimem___024root__Slow.cpp \
- Vimem___024root__DepSet_hd637b107__0__Slow.cpp Vimem__Syms.cpp
