275b09f68c41988b97a42d127d6a920441eb939c
FlavioGazzetta <flavio.gazzetta.2005@gmail.com>
2024-12-08 02:19:42 +0000

    futher changes to pipelined cpu in order to make progress in tasks 2-5

--- rtl/PRegFetch.sv
+++ rtl/PRegFetch.sv
@@ -5,7 +5,7 @@ module PRegFetch #(
     input   logic [DATA_WIDTH-1:0]  PCf,
     input   logic [DATA_WIDTH-1:0]  PCPlus4F,
     input   logic                   clk,
-    input   logic                   FlushF,
+    input   logic                   FlushD,
     input   logic                   rst,         // Reset signal
     output  logic [DATA_WIDTH-1:0]  PCPlus4D,
     output  logic [DATA_WIDTH-1:0]  PCd,
@@ -14,7 +14,7 @@ module PRegFetch #(
 
     // Sequential logic for writing and output generation
     always_ff @(posedge clk or posedge rst) begin
-        if (rst || FlushF) begin
+        if (rst || FlushD) begin
             // Reset or flush: Set outputs to default values
             InstrD   <= 32'b0;
             PCd      <= 32'b0;
