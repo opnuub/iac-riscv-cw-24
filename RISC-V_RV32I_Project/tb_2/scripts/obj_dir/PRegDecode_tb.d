3bb89f6b99f94b132ba3aca5e23b2e170a212fac
FlavioGazzetta <flavio.gazzetta.2005@gmail.com>
2024-12-10 06:37:38 +0000

    deleted files not relevant to pipeline

--- RISC-V_RV32I_Project/tb_2/scripts/obj_dir/PRegDecode_tb.d
+++
@@ -1,7 +0,0 @@
-PRegDecode_tb.o: \
- /home/fg723/Documents/iac/lab0-devtools/autumn/workspace/iac-riscv-cw-24/RISC-V_RV32I_Project/tb/tests/PRegDecode_tb.cpp \
- VPRegDecode.h /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
