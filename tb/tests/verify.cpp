8a7b3cefe7045a8c57bcc44e2fd9813a4bedaa2c
Michael Li <mcl123@ic.ac.uk>
2024-12-14 01:36:14 +0000

    Push

--- tb/tests/verify.cpp
+++ tb/tests/verify.cpp
@@ -38,7 +38,7 @@ TEST_F(CpuTestbench, TestJalRet)
 TEST_F(CpuTestbench, TestPdf)
 {
     setupTest("5_pdf");
-    setData("/Users/michaelli/Documents/GitHub/iac-riscv-cw-24/tb/reference/gaussian.mem");
+    setData("tb/reference/gaussian.mem");
     initSimulation();
     runSimulation(CYCLES * 100);
     EXPECT_EQ(top_->a0, 15363);
